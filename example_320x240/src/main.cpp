963fc7190396cc3f45f7dfad4cdb9c25fb215f45
James Kong <fishkingsin@gmail.com>
2014-06-10 12:02:54 +0800

    update app setting for 0.8.1

--- example_320x240/src/main.cpp
+++ example_320x240/src/main.cpp
@@ -1,20 +1,27 @@
 #include "ofMain.h"
 #ifdef TARGET_LINUX_ARM
-#include "ofGLES2Renderer.h"
+#include "ofAppEGLWindow.h"
 #endif
 #include "testApp.h"
 
 //========================================================================
 int main( ){
 #ifdef TARGET_LINUX_ARM
-	ofGLES2Renderer * renderer = new ofGLES2Renderer();
-	ofSetCurrentRenderer(ofPtr<ofBaseRenderer>(renderer));
-#endif
-	ofSetupOpenGL(320,240, OF_WINDOW);			// <-------- setup the GL context
+	ofAppEGLWindow::Settings settings;
     
+	settings.eglWindowOpacity = 127;
+    settings.frameBufferAttributes[EGL_DEPTH_SIZE]   = 0; // 0 bits for depth
+    settings.frameBufferAttributes[EGL_STENCIL_SIZE] = 0; // 0 bits for stencil
+    
+	ofAppEGLWindow window(settings);
+    
+	ofSetupOpenGL(&window, 1024,768, OF_FULLSCREEN);// <-------- setup the GL context
+#else
+	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+#endif
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp( new testApp());
-    
+
 }
